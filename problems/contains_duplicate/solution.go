3ec1da1e8fc07b05a38c331c552d4630938a54f9
Cong Wang <findcongwang@gmail.com>
2019-11-23 14:32:24 +0000

    Sync LeetCode submission - Contains Duplicate (golang)

---
+++ problems/contains_duplicate/solution.go
@@ -0,0 +1,11 @@
+func containsDuplicate(nums []int) bool {
+    set := map[int]bool{}
+    
+    for _, v := range(nums) {
+        if set[v] {
+            return true
+        }
+        set[v] = true
+    }
+    return false
+}
