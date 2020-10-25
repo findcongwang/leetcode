ce001194d746569028f384217e7df29e65781bef
Cong Wang <findcongwang@gmail.com>
2020-10-25 03:26:05 +0000

    Sync LeetCode submission - Univalued Binary Tree (python3)

---
+++ problems/univalued_binary_tree/solution.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: TreeNode) -> bool:
+        # dfs, check all val equal to root
+        return self.isUnivalTreeHelper(root, root.val)
+    
+    def isUnivalTreeHelper(self, node, val):
+        return (
+            node.val == val
+            and (node.left is None or self.isUnivalTreeHelper(node.left, val))
+            and (node.right is None or self.isUnivalTreeHelper(node.right, val))
+        )
