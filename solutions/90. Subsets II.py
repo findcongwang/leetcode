206840bf3e528853158cf772d52df24c7ca626b3
Cong Wang <findcongwang@gmail.com>
2021-01-09 02:41:30 -0500

    36 ms

---
+++ solutions/90. Subsets II.py
@@ -0,0 +1,7 @@
+from itertools import combinations, chain
+​
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        powerset = chain.from_iterable([combinations(nums, i) for i in range(len(nums)+1)])
+        return list(set(powerset))
