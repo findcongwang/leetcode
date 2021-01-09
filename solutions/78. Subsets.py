ed38581b64968ace14dc36f2269b46b271f79911
Cong Wang <findcongwang@gmail.com>
2021-01-09 02:41:15 -0500

    36 ms

---
+++ solutions/78. Subsets.py
@@ -0,0 +1,7 @@
+from itertools import combinations, chain
+​
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        powerset = chain.from_iterable([combinations(nums, i) for i in range(len(nums)+1)])
+        return list(powerset)
+​
