0c9f0b9b8e29a49b2a7141dd07ff16cc700d8f41
Cong Wang <findcongwang@gmail.com>
2021-01-23 00:11:39 +0000

    Sync LeetCode submission - Merge Sorted Array (python3)

---
+++ problems/merge_sorted_array/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # assign from the back
+        i, j = m-1, n-1
+        idx = len(nums1)-1
+        
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[idx] = nums1[i]
+                i -= 1
+            else:
+                nums1[idx] = nums2[j]
+                j -= 1
+            idx -= 1
+            
+        while j >= 0:            
+            nums1[idx] = nums2[j]
+            idx -= 1
+            j -= 1
+    
+            
+            
